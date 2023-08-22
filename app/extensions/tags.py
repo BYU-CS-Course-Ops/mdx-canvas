9d587f356c0f4f57c48da4b8e918cbd035cc2d3c
prestonraab <prestonraab@gmail.com>
2023-08-22 09:53:42 -0600

    Use Beautiful soup to process html tags

--- app/extensions/tags.py
+++ app/extensions/tags.py
@@ -3,9 +3,9 @@ import re
 from markdown.extensions import Extension
 from markdown.preprocessors import Preprocessor
 from markdown.postprocessors import Postprocessor
-from markdown.preprocessors import HtmlBlockPreprocessor
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
+
 
 def makeExtension(configs=None):
     if configs is None:
@@ -21,12 +21,6 @@ class TagsExtension(Extension):
         super().__init__(**kwargs)
 
     def extendMarkdown(self, md, md_globals):
-        list_class = self.getConfig("list_class")
-        renderer = self.getConfig("render_item")
-        # md.preprocessors.add(
-        #     'html_block',
-        #     HtmlBlockPreprocessor(md),
-        #     '>normalize_whitespace')
         preprocessor = TagsPreprocessor(md)
         md.preprocessors.add("tags", preprocessor, '>normalize_whitespace')
 
@@ -35,10 +29,6 @@ class TagsPreprocessor(Preprocessor):
     """
     Works html tags into check and radio boxes
     """
-
-    list_pattern = re.compile(r"(<ul>\n<li>\([ Xx]\))")
-    item_pattern = re.compile(r"^<li>\(([ Xx])\)(.*)</li>$", re.MULTILINE)
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -47,26 +37,26 @@ class TagsPreprocessor(Preprocessor):
 
         soup = BeautifulSoup("\n".join(lines), "html.parser")
         questions = soup.find_all('question')
+        index = 1
+
         for question in questions:
-            print(question.contents)
-            print(question.right)
-            question.replace_with()
+            question.contents[0].replace_with(f"{index}. {question.contents[0][1:]}")
+            print(question.string)
+            rights = question.css.filter('right')
 
-        html = re.sub(self.list_pattern, self._convert_list, html)
-        return re.sub(self.item_pattern, self._convert_item, html)
+            one_correct = len(rights) == 1
+            for right in rights:
+                without_newlines = right.string.replace('\n', ' ')
+                parens = "(x)" if one_correct else "[x]"
+                right.replace_with(f"    - {parens} {without_newlines}")
+            for wrong in question.css.filter('wrong'):
+                without_newlines = wrong.string.replace('\n', ' ')
+                parens = "( )" if one_correct else "[ ]"
+                wrong.replace_with(f"    - {parens} {without_newlines}")
 
-    def _convert_list(self, match):
-        return match.group(1).replace("<ul>", f"<ul class=\"{self.list_class}\">")
+            question.replace_with("".join(question.contents))
+            index += 1
 
-    def _convert_item(self, match):
-        state, caption = match.groups()
-        return self.render_item(caption, state != " ")
-
-
-def render_item(caption, checked):
-    correct = "1" if checked else "0"
-    fake = "0" if checked else "1"
-
-    return f"<li>" \
-           f"<label><input type=\"radio\" data-question=\"{fake}\" data-content=\"{correct}\" />{caption}</label>" \
-           f"</li>"
+        result = str(soup)
+        print(result)
+        return result.split("\n")
--- markdown-quiz-files/sample-quiz.md
+++ markdown-quiz-files/sample-quiz.md
@@ -10,31 +10,58 @@
     - ( ) False 
 
 3. What topics are taught in CS 110?
-    - [x] Lists
+    - [x] Lists 
     - [x] Functions
     - [ ] Classes
-	- [ ] Recursion
+	- [ ] ```import random```
 
 4. Who is the president of BYU?
     - R:= C. Shane Reese
 
-<Quiz>
-
+---
 <question>
 This is a text question
-<right>Yes</right>
 </question>
 
-just some text
 
 <question>
-<right>Blue</right>
-Question 1:
 This is the question part
-<right>Blue</right>
+
+<right>
+Blue
+Red
+Orange
+</right>
+
 <wrong>Green</wrong>
 <wrong>Purple</wrong>
 </question>
 
-</Quiz>
+
+<question>
+This is another question
+
+<right>5</right>
+<right>6</right>
+
+<wrong>This is a very long
+multiline answer
+That could be displayed improperly;
+I can see how
+Multiline coding answers could potentially
+be problematic for parsing
+And displaying as html </wrong>
+
+<wrong>
+
+```python
+import random
+
+def give_me_100():
+    return random.random() * 100
+```
+
+</wrong>
+</question>
+
 
