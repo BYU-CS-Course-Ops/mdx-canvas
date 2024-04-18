efa04a31b63133b86a79f04431e1de55333e9e56
Robert Greathouse <robbykap@byu.edu>
2024-04-17 19:27:16 -0600

    Created a version of JinjaParser which allows for muliple templates for an assignment "group"

--- mdxcanvas/jinja_parser.py
+++ mdxcanvas/jinja_parser.py
@@ -1,3 +1,4 @@
+import re
 import csv
 import json
 import jinja2 as jj
@@ -5,17 +6,40 @@ from pathlib import Path
 from itertools import takewhile
 
 
-def _process_jinja(template: jj.Template, global_args, arg_sets) -> str:
+def _process_multi_templates(template_folder, global_args, arg_sets):
+    folder = jj.FileSystemLoader(template_folder)
+    jj_templates = jj.Environment(loader=folder)
+    global_args |= dict(zip=zip, split_list=lambda x: x.split(';'))
+
     content = []
-    for args in arg_sets:
-        content.append(template.render(
-            **global_args,
-            **args
-        ))
+    for arg_set in arg_sets:
+        jj_template = jj_templates.get_template(arg_set.get('Filename'))
+
+        content.append(
+            jj_template.render(
+                **global_args,
+                **arg_set
+            ))
+
     return '\n'.join(content)
 
 
-def process_jinja(template_file: Path) -> str:
+def _process_single__template(template, global_args, arg_sets):
+    jj_template = jj.Environment().from_string(template)
+    global_args |= dict(zip=zip, split_list=lambda x: x.split(';'))
+
+    content = []
+    for arg_set in arg_sets:
+        content.append(
+            jj_template.render(
+                **global_args,
+                **arg_set
+            ))
+
+    return '\n'.join(content)
+
+
+def process_jinja(template_file: Path, **kwargs) -> str:
     """
     Takes a jinja template file and renders it with the given arguments.
     The template file contains comments which store the global and local arguments.
@@ -23,9 +47,9 @@ def process_jinja(template_file: Path) -> str:
     :param template_file: The path to the jinja template file
     :return: The rendered jinja template as a string
 
-    Examples:
+    Example:
     ----------
-    >>> process_jinja(Path("template.jinja")
+    >>> process_jinja(Path("template.jinja"))
     "<assignment
         title="Day 1"
         description="This is the first day of the course"
@@ -41,7 +65,7 @@ def process_jinja(template_file: Path) -> str:
     """
     template = template_file.read_text()
 
-    args_template = jj.Environment().from_string('\n'.join(takewhile(lambda x: x, template.splitlines())))
+    args_template = jj.Environment().from_string('\n'.join(takewhile(lambda x: re.match(r' *{% *set', x), template.splitlines())))
     template_args = args_template.module.__dict__
 
     global_args_file = (template_file.parent / template_args.get('global')).resolve()
@@ -50,10 +74,13 @@ def process_jinja(template_file: Path) -> str:
     args_file = (template_file.parent / template_args.get('args')).resolve()
     arg_sets = list(csv.DictReader(args_file.read_text().splitlines()))
 
-    jj_template = jj.Environment().from_string(template)
-    global_args |= dict(zip=zip, split_list=lambda x: x.split(';'))
-    return _process_jinja(jj_template, global_args, arg_sets)
+    if 'templates' in template_args:
+        template_folder = template_file.parent.resolve()
+        return _process_multi_templates(template_folder, global_args, arg_sets)
+    else:
+        return _process_single__template(template, global_args, arg_sets)
 
 
 if __name__ == '__main__':
-    print(process_jinja(Path("../demo_course/public-files/template-material/DayTemplate.jinja")))
+    # print(process_jinja(Path("../demo_course/public-files/template-material/reading-quiz-templates/ReadingQuiz.canvas.canvas.xml.jinja")))
+    print(process_jinja(Path("../demo_course/public-files/template-material/HwTemplate.canvas.xml.jinja")))
