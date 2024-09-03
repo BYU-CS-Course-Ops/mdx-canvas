bea219d38065675a20f0275bba68cb740a6f4284
Gordon Bean <gbean@cs.byu.edu>
2024-09-03 15:40:37 -0600

    add back in inline-code black style

--- mdxcanvas/extensions.py
+++ mdxcanvas/extensions.py
@@ -1,7 +1,3 @@
-import re
-import xml.etree.ElementTree as etree
-
-from markdown.inlinepatterns import BacktickInlineProcessor, BACKTICK_RE
 from markdown.extensions import Extension
 
 from markdown.postprocessors import Postprocessor
@@ -12,22 +8,6 @@ from bs4.element import Tag
 from typing import Protocol
 
 from mdxcanvas.xml_processing.inline_styling import get_style, parse_css, apply_inline_styles
-
-
-class BlackInlineCodeProcessor(BacktickInlineProcessor):
-    def handleMatch(self, m: re.Match[str], data: str) -> tuple[etree.Element | str, int, int]:
-        el, start, end = super().handleMatch(m, data)
-        el.attrib['style'] = 'color: #000000'
-        return el, start, end
-
-
-class BlackInlineCodeExtension(Extension):
-    def extendMarkdown(self, md):
-        # We use 'backtick' and 190 which are the same values
-        # used in markdown.inlinepatterns.py to register the original
-        # BacktickInlineCodeProcessor.
-        # By reusing the same name, it overrides the original processor with ours
-        md.inlinePatterns.register(BlackInlineCodeProcessor(BACKTICK_RE), 'backtick', 190)
 
 
 # Make a Protocol for any tag processor, it should take a Tag and return a Tag
