ef908bcc5955fd00f25e33a1d7d6f55ff9f23734
Robert Greathouse <robbykap@byu.edu>
2025-02-05 12:25:16 -0700

    Small refactor

--- mdxcanvas/deploy/groups.py
+++ mdxcanvas/deploy/groups.py
@@ -4,12 +4,8 @@ from ..our_logging import get_logger
 logger = get_logger()
 
 
-def get_groups(course: Course) -> list[AssignmentGroup]:
-    return course.get_assignment_groups()
-
-
 def get_group(course: Course, group_name: str) -> AssignmentGroup | None:
-    for group in get_groups(course):
+    for group in course.get_assignment_groups():
         if group.name == group_name:
             return group
     return None
@@ -24,8 +20,12 @@ def update_group(group: AssignmentGroup, **kwargs) -> AssignmentGroup:
 
 
 def initialize_group_weights(course: Course, group_weights: dict):
+    # Update the course to apply the assignment group weights
     course.update(course={'apply_assignment_group_weights': True})
+
+    # Set the group weights
     for group in group_weights:
+        # Check if the group exists
         if (canvas_group := get_group(course, group)) is None:
             logger.info(f'Creating group: {group}')
             canvas_group = create_group(course, group)
