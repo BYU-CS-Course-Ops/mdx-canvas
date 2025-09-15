a09797c57b72f96b1abcd3c0675bae55865c85db
robbykap <robbykap@byu.edu>
2025-09-15 09:00:19 -0600

    WIB assignment group tag
    Started the assignment group tag and have main parts stubbed out

--- mdxcanvas/xml_processing/assignment_groups_tag.py
+++ mdxcanvas/xml_processing/assignment_groups_tag.py
@@ -1,36 +1,50 @@
-# TODO
-# <assignment_groups>
-# | Group | Weight |
-# |-------|--------|
-# | Homework | 10%  |
-# | Projects | 40%  |
-# | Exams    | 10%  |
-# </assignment_groups>
-# Parse that table into a dictionary
-# str: int
+from bs4 import Tag
+from canvasapi.course import Course
+from canvasapi.assignment import AssignmentGroup
 
-# To deploy:
-# Using the course.update() method,
-# set the 'apply_assignment_group_weights' field to True
-#
-# # Get all assignment groups for the course
-# assignment_groups = course.get_assignment_groups()
-#
-# Create the missing groups
-#
-# Map all names to IDs
-#
-# # Define the weights you want to set
-# # Note: This should be a dict mapping group ids to their corresponding weights
-# weights = {
-#     'Homework': 30,
-#     'Projects': 50,
-#     'Exams': 20,
-# }
-#
-# # Update the weights for each assignment group
-# for group in assignment_groups:
-#     group_name = group.name  # is this field available?
-#     if group_name in weights:
-#         new_weight = weights[group_name]
-#         updated_group = group.edit(assignment_group={'group_weight': new_weight})
+from .attributes import Attribute, parse_bool, parse_settings, parse_int
+from ..resources import ResourceManager, get_key, CanvasResource
+
+class AssignmentGroupsTagProcessor:
+    """
+    Processes <assignment-groups> tags.
+
+    Usage:
+        <assignment-groups>
+            <group name="Group 1" weight="25" drop="5" />
+            <group name="Group 2" weight="75" drop="3" />
+        </assignment-groups>
+    """
+    def __init__(self, resource_manager: ResourceManager):
+        self._resources = resource_manager
+
+    def __call__(self, assignment_groups_tag: Tag):
+        for group_tag in assignment_groups_tag.find_all('group'):
+            self._parse_assignment_group(group_tag)
+
+    def _parse_assignment_group(self, tag: Tag):
+        fields = [
+            Attribute('name', required=True),
+            Attribute('weight', parser=parse_int),
+            Attribute('drop', 0, parse_int),
+            Attribute('keep', None, parse_int),
+            Attribute('position', None, parse_int),
+            Attribute('group_category'),
+            Attribute('rules', {}, parser=parse_settings),  # Additional rules can be added here
+            Attribute('published', False, parse_bool)
+        ]
+
+        group_data = parse_settings(tag, fields)
+
+        group_name = group_data.pop('name')
+
+        assignment_group = CanvasResource(
+            type='assignment_group',
+            name=group_name,
+            data=group_data
+        )
+        self._resources.add_resource(assignment_group)
+
+        # Optionally, link the group to a course if specified
+
+
