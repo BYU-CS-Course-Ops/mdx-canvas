509e6a5b5874b6e4d6352612c498208b68b2cba0
Gordon Bean <gbean@cs.byu.edu>
2025-12-20 20:21:01 -0700

    0.5.5 - use URL (http_url) for course-links

--- mdxcanvas/xml_processing/tag_preprocessors.py
+++ mdxcanvas/xml_processing/tag_preprocessors.py
@@ -237,11 +237,11 @@ def make_link_preprocessor():
         link_rid = tag['id']
 
         new_tag = Tag(name='a')
-        new_tag['href'] = get_key(link_type, link_rid, 'uri')
+        new_tag['href'] = get_key(link_type, link_rid, 'url')
         # TODO - add other course-link attributes here
         link_text = tag.string.strip() if tag.string is not None else ''
         if not link_text:
-            link_text = f'{link_rid}'
+            link_text = str(link_rid)
         new_tag.string = link_text
         tag.replace_with(new_tag)
 
