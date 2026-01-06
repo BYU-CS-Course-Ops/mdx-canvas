3e7bac42555dc2070278b1bebfa3ca70f3fbc5ee
robbykap <robbykap@byu.edu>
2026-01-05 18:06:00 -0700

    Update override.py

--- mdxcanvas/deploy/override.py
+++ mdxcanvas/deploy/override.py
@@ -3,18 +3,6 @@ from canvasapi.course import Course
 
 from ..resources import OverrideInfo
 
-# def deploy_quiz_override(course: Course, quiz_id: int, override_info: dict)-> tuple[AssignmentOverride, str | None]:
-#     override_set = course.get_quiz_overrides(**{"quiz_assignment_overrides[][quiz_ids][]": quiz_id})[0]
-#
-#     if override_set:
-#         for override in override_set.overrides:
-#             override.edit(**override_info)
-#     else:
-#         quiz = course.get_quiz(quiz_id)
-#         override_set = quiz.create_override(**override_info)
-#
-#     return override_set, None
-
 
 def deploy_override(course: Course, override_info: dict) -> tuple[OverrideInfo, None]:
     rtype = override_info.get('rtype')
