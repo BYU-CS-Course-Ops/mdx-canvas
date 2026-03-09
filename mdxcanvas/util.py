91c1aa4823522f42ec995468c81d75777eab71ab
Ethan Dye <mrtops03@gmail.com>
2026-03-09 15:50:01 -0600

    Use tuple default
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

--- mdxcanvas/util.py
+++ mdxcanvas/util.py
@@ -16,7 +16,7 @@ def parse_soup_from_xml(text: str) -> BeautifulSoup:
     return BeautifulSoup(text, 'html.parser')
 
 
-def retrieve_contents(tag: Tag, ignored_child_tag_names: list[str] = []) -> str:
+def retrieve_contents(tag: Tag, ignored_child_tag_names: list[str] = ()) -> str:  # type: ignore[reportIncompatibleVariableOverride]
     """
     Return all the HTML contents of the specified tag
     Excludes the contents of specific sub-tags.
